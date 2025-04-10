a08ac7f6c4f0ea1922d15e29f075f39a6cf57801
sanakassem20 <kassemsana00@gmail.com>
2025-04-10 14:39:45 +0300

    sxs

--- FashionBoutiqueLogin/Form1.cs
+++ FashionBoutiqueLogin/Form1.cs
@@ -29,30 +29,26 @@ namespace FashionBoutiqueLogin
                 cmd.Parameters.AddWithValue("@Password", password);
 
                 SqlDataReader reader = cmd.ExecuteReader();
+
                 if (reader.Read())
                 {
                     string role = reader["Role"].ToString();
                     string firstName = reader["FirstName"].ToString();
-                    string ssn = reader["SSN"].ToString(); // Retrieve the SSN
+                    string ssn = reader["SSN"].ToString();
 
                     lblMessage.ForeColor = System.Drawing.Color.Green;
                     lblMessage.Text = $"Welcome {firstName}!";
 
-                    if (role == "Admin")
-                    {
-                        this.Hide();
+                    this.Hide();
 
-                        // Pass both SSN and Role to the Main form
-                        Main mainForm = new Main(ssn, role);
-                        this.Hide();  // Hide the login form
-                        mainForm.ShowDialog();  // Open the main form
-                        this.Show();  // Show the login form again after closing main form
-                    }
-                    else
-                    {
-                        lblMessage.ForeColor = System.Drawing.Color.Red;
-                        lblMessage.Text = "Invalid Username or Password!";
-                    }
+                    Main mainForm = new Main(ssn, role);
+                    mainForm.ShowDialog();
+                    this.Show();
+                }
+                else
+                {
+                    lblMessage.ForeColor = System.Drawing.Color.Red;
+                    lblMessage.Text = "Invalid Username or Password!";
                 }
             }
             catch (Exception ex)
@@ -68,6 +64,7 @@ namespace FashionBoutiqueLogin
                 }
             }
         }
+
 
         private void picxbox_Click(object sender, EventArgs e)
         {
